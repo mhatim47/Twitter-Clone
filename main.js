8cbec9b9e816560a435a47e7858c65c19ee0a681
mhatim47 <mohamedhatime47@gmail.com>
2026-07-03 18:11:19 +0300

    added images

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-import {tweetsData} from 'data.js'
-console.log(tweetsData)
+import {tweetsData} from './data.js'
+
+const tweetBtn = document.getElementById("tweet-btn");
+const tweetInput = document.getElementById("tweet-input");
+
+tweetBtn.addEventListener("click", function() {
+    console.log(tweetInput.value)
+})
