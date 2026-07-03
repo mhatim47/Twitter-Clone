60d0e74e9d357b882a06e921e7800b7604a3e4a8
mhatim47 <mohamedhatime47@gmail.com>
2026-07-03 18:28:19 +0300

    Added Tweet Boilerplate

--- main.js
+++ main.js
@@ -5,4 +5,36 @@ const tweetInput = document.getElementById("tweet-input");
 
 tweetBtn.addEventListener("click", function() {
     console.log(tweetInput.value)
+    getFeedHtml()
 })
+
+function getFeedHtml() {
+
+    let feedHtml = ``
+
+    for (const tweet of tweetsData) {
+        feedHtml += `
+            <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
+            </div>
+    </div>
+        `
+    }
+    console.log(feedHtml)
+}
